8712235f1585c5fc4aa253179c326f39d195c6c5
Matheus Ramos <matheusdasilvaramos10@gmail.com>
2022-09-07 12:34:50 -0300

    Server middlewares fixed

--- src/index.js
+++ src/index.js
@@ -10,19 +10,51 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(item => item.username === username)
+  if (!user) return response.status(404).json({ error: "User not found" })
+
+  request.user = user
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro || user.todos.length !== 10)
+    next()
+  else
+    return response.status(403).json({ error: 'Free plan limit reached, subscribe to pro plan create more todos' })
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const isValidUuid = validate(id)
+  if (!isValidUuid) return response.status(400).json({ error: "Invalid ID format" })
+
+  const user = users.find(item => item.username === username)
+  if (!user) return response.status(404).json({ error: "User not found" })
+  
+  const todo = user.todos.find(item => item.id === id)
+  if (!todo) return response.status(404).json({ error: "Todo not found" })
+
+  request.user = user
+  request.todo = todo
+
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(item => item.id === id)
+  if (!user) return response.status(404).json({ error: "User not found" }) 
+
+  request.user = user
+  next();
 }
 
 app.post('/users', (request, response) => {
